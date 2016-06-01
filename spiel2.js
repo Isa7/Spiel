f9f8a3aee245eba9271b793d3ebd3f8de2e96594
Isa7 <isa.singler@freenet.de>
2016-06-01 11:59:16 +0200

    Revert "Tastensteuerung"
    This reverts commit 8d2ebe0b12c9fb286b1baa00381e451fd8db97de.

--- spiel2.js
+++ spiel2.js
@@ -28,13 +28,3 @@ function start(){
 	
 // Tasten abfragen und Schlange steuern
 // Kontakt mit Apfel erkennen
-
-
-//Variablen für Tastensteuerung
-
-var Key = {
-	LEFT: 37,
-	UP: 38,
-	RIGHT: 39,
-	DOWN: 40,
-};
